2e3cd62b46aa5b47d0071898311e0ded8a3ce88a
Mamun <>
2024-02-17 14:58:03 +0600

    Queue with Array impemented

--- DSA Queue/DSA Queue/main.swift
+++ DSA Queue/DSA Queue/main.swift
@@ -7,5 +7,55 @@
 
 import Foundation
 
-print("Hello, World!")
 
+func block(_ title: String, execution: ()->()){
+    print("\n --- \(title) ---")
+    execution()
+}
+
+
+protocol Queue {
+    associatedtype Element
+        
+    mutating func enqueue(_ element: Element)
+    
+    mutating func dequeue()->Element?
+    
+    var isEmpty: Bool { get }
+    
+    var peek: Element? { get }
+}
+
+
+struct QueueArray<T>: Queue {
+   private var array: [T] = []
+    
+    init(){ }
+        
+    var isEmpty: Bool { array.isEmpty }
+    
+    var peek: T? { array.first }
+    
+    mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func dequeue()->T? {
+        isEmpty ? nil : array.removeFirst()
+    }
+}
+
+extension QueueArray: CustomStringConvertible {
+    var description: String {
+        "--- queue start ---\n" + array.map { "\($0)" }.joined(separator: " -> ") + "\n--- queue end ---"
+    }
+}
+
+var queue = QueueArray<Int>()
+queue.enqueue(5)
+queue.enqueue(7)
+queue.enqueue(9)
+queue.enqueue(11)
+
+print(queue.description)
+print(queue.peek)
